e90aebf606aa4810461a0a9d0cb6f3dc9d82f741
Nikita <nikitakuydin@gmail.com>
2023-07-09 20:30:06 +0700

    Pervoe prilogenie na django

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-     return HttpResponse("Успешно")
+     return render(request, 'index.html')
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
+
