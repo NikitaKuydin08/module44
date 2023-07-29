eddef47752f4b34a5cf67b1b33191a38ac26c957
Nikita <nikitakuydin@gmail.com>
2023-07-29 17:45:15 +0700

    Лекция номер 8

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
 class Meta:
     db_table = '"advertisements"'
@@ -30,7 +32,16 @@ class Advertisement(models.Model):
     # Дата изменения обновления
     # Поле записывается при каждом обновлении
     updated_at = models.DateTimeField(auto_now=True)
- 
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_date = self.created_at.strftime("%H:%M:%S")
+            return format_html('<span style="color:green; font-weight:bold;">Сегодня в {} </span>', created_date)
+        
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
 
